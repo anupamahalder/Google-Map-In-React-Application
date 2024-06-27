42c134335f744aa4ecdec44c180867bf2f4120ec
anupamahalder <anupamahalder2022@gmail.com>
2024-06-27 12:56:01 +0530

    Added pin to the location which is comming from the input field

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import MapPage from './MapPage'
 import SimpleMap from './SimpleMap'
-import {APIProvider, Map, AdvancedMarker} from '@vis.gl/react-google-maps';
+import {APIProvider, Map, AdvancedMarker, Pin, InfoWindow} from '@vis.gl/react-google-maps';
 
 const App = () =>{
   const [count, setCount] = useState(0);
   const position = {lat: 53.54, lng: 10};
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <div>
@@ -29,7 +31,14 @@ const App = () =>{
             mapId={process.env.MAP_ID}
             // disableDefaultUI={true}
           >
-            <AdvancedMarker position={position}></AdvancedMarker>
+            <AdvancedMarker position={position} onClick={() => setOpen(!open)}>
+            <Pin background={"red"} borderColor={"green"} glyphColor={"blue"}></Pin>
+            </AdvancedMarker>
+            {
+              open && <InfoWindow position={position}>
+                <p style={{color:"black"}}>I am in Humbarg</p>
+              </InfoWindow>
+            }
           </Map>
         </div>
       </APIProvider>
